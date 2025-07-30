a3f3a32d57de1bdc7f44035ce0d44deea1e65d5f
Tolotra <rasamoelilala.tolotra@gmail.com>
2025-07-30 09:44:35 +0100

    forcer afficher navbar

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,72 +1,71 @@
 
 
-const texts = ["Développeur Front-end", "Web Designer UI/UX", "Intégrateur"];
-let index = 0; 
-let charIndex = 0;
-let isDeleting = false;
+// const texts = ["Développeur Front-end", "Web Designer UI/UX", "Intégrateur"];
+// let index = 0; 
+// let charIndex = 0;
+// let isDeleting = false;
 
-const typedTextEl = document.getElementById("typedText");
+// const typedTextEl = document.getElementById("typedText");
 
-function typeEffect() {
-  const currentText = texts[index];
+// function typeEffect() {
+//   const currentText = texts[index];
 
-  if (!isDeleting && charIndex <= currentText.length) {
-    typedTextEl.textContent = currentText.substring(0, charIndex);
-    charIndex++;
-    setTimeout(typeEffect, 100);
-  } else if (isDeleting && charIndex >= 0) {
-    typedTextEl.textContent = currentText.substring(0, charIndex);
-    charIndex--;
-    setTimeout(typeEffect, 50);
-  } else {
-    isDeleting = !isDeleting;
-    if (!isDeleting) index = (index + 1) % texts.length;
-    setTimeout(typeEffect, 1000);
-  }
-}
+//   if (!isDeleting && charIndex <= currentText.length) {
+//     typedTextEl.textContent = currentText.substring(0, charIndex);
+//     charIndex++;
+//     setTimeout(typeEffect, 100);
+//   } else if (isDeleting && charIndex >= 0) {
+//     typedTextEl.textContent = currentText.substring(0, charIndex);
+//     charIndex--;
+//     setTimeout(typeEffect, 50);
+//   } else {
+//     isDeleting = !isDeleting;
+//     if (!isDeleting) index = (index + 1) % texts.length;
+//     setTimeout(typeEffect, 1000);
+//   }
+// }
 
-document.addEventListener("DOMContentLoaded", typeEffect);
+// document.addEventListener("DOMContentLoaded", typeEffect);
 
 
-window.addEventListener('load', () => {
-    // Retirer après 3 secondes (3000 ms)
-    setTimeout(() => {
-      document.body.classList.remove('body-lock');
-    }, 2000);
+// window.addEventListener('load', () => {
+//     setTimeout(() => {
+//       document.body.classList.remove('body-lock');
+//     }, 2000);
 
-        const navbar = document.querySelector('.navbar');
-        navbar.classList.remove('hidden-before-load');
-        navbar.classList.add('visible-after-load');
-    });
-    function animateProgressBars() {
-        const section = document.querySelector("#technologie");
-        const bars = section.querySelectorAll(".progress-bar");
+//         const navbar = document.querySelector('.navbar');
+//         navbar.classList.remove('hidden-before-load');
+//         navbar.classList.add('visible-after-load');
+//     });
+//     function animateProgressBars() {
+//         const section = document.querySelector("#technologie");
+//         const bars = section.querySelectorAll(".progress-bar");
 
-        let sectionVisible = false;
+//         let sectionVisible = false;
 
-        function isInViewport(el) {
-            const rect = el.getBoundingClientRect();
-            return (
-                rect.top < window.innerHeight && rect.bottom > 0
-            );
-        }
+//         function isInViewport(el) {
+//             const rect = el.getBoundingClientRect();
+//             return (
+//                 rect.top < window.innerHeight && rect.bottom > 0
+//             );
+//         }
 
-        function triggerAnimation() {
-            if (sectionVisible) return;
-            if (isInViewport(section)) {
-                sectionVisible = true;
+//         function triggerAnimation() {
+//             if (sectionVisible) return;
+//             if (isInViewport(section)) {
+//                 sectionVisible = true;
 
-                bars.forEach(bar => {
-                    const target = parseInt(bar.getAttribute("data-percentage"));
-                    bar.style.transition = "width 2.5s ease-in-out";
-                    bar.style.width = target + "%";
-                    bar.setAttribute("aria-valuenow", target);
-                });
-            }
-        }
+//                 bars.forEach(bar => {
+//                     const target = parseInt(bar.getAttribute("data-percentage"));
+//                     bar.style.transition = "width 2.5s ease-in-out";
+//                     bar.style.width = target + "%";
+//                     bar.setAttribute("aria-valuenow", target);
+//                 });
+//             }
+//         }
 
-        document.addEventListener("scroll", triggerAnimation);
-        window.addEventListener("load", triggerAnimation);
-    }
+//         document.addEventListener("scroll", triggerAnimation);
+//         window.addEventListener("load", triggerAnimation);
+//     }
 
-    animateProgressBars();
+//     animateProgressBars();
